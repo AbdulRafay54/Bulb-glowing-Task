9dcdcac92df47314e663037353c7f6ad96baf402
AbdulRafay54 <rafayabdul7817@gmail.com>
2023-11-20 10:01:50 +0500

    first comment

---
+++ app.js
@@ -0,0 +1,13 @@
+function swapImage() {
+    let img = document.querySelector("#bulb-off");
+    img.src = "./bulb-on.jpg"
+
+}
+function backToOriginal() {
+    let img = document.querySelector("#bulb-off");
+    img.src = "./bulb-of.jpg"
+}
+ 
+
+ 
+
